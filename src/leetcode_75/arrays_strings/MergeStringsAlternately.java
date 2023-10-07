a4d907fe19b3c7fd37b539d50c3327a38697dec8
Sardul <27910681+thesardul@users.noreply.github.com>
2023-10-07 23:26:57 +0530

    1768. Merge Strings Alternately
    1768. Merge Strings Alternately
    
    https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/leetcode_75/arrays_strings/MergeStringsAlternately.java
@@ -0,0 +1,25 @@
+package leetcode_75.arrays_strings;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        System.out.println(mergeAlternately(word1, word2));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if(i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return result.toString();
+    }
+}
